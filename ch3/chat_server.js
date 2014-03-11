e46d00aa97e690f52aa85d81d74aefec96ef4806
Jose Bueno <kdebrain@gmail.com>
2014-03-11 13:48:10 -0400

    pg 55

--- ch3/chat_server.js
+++ ch3/chat_server.js
@@ -6,7 +6,10 @@ channel.clients = {};
 channel.subscriptions = {};
 
 channel.on('join', function(id, client) {
-	console.log("Welcome " + id);
+	var welcome = "Welcome " + id
+							+ ' Guests Online: ' + this.listeners('broadcast').length;
+	client.write(welcome + '\n');
+
 	this.clients[id] = client;
 	this.subscriptions[id] = function(senderId, message) {
 		if(id != senderId) {
@@ -15,6 +18,18 @@ channel.on('join', function(id, client) {
 	};
 	this.on('broadcast', this.subscriptions[id]);
 });
+
+channel.on('leave', function(id) {
+		this.removeListener('broadcast', this.subscriptions[id]);
+		this.emit('broadcast', id, id + ' has left the chat.\n');
+});
+
+channel.on('shutdown', function() {
+		this.emit('broadcast', '', 'Chat has shut down.\n');
+		this.removeAllListeners('broadcast');
+});
+
+channel.setMaxListeners(50);
 
 var server = net.createServer(function(client) {
 	var id = client.remoteAddress + ':' + client.remotePort;
@@ -29,7 +44,15 @@ var server = net.createServer(function(client) {
 
 	client.on('data', function(data) {
 		data = data.toString();
+		if(data == 'shutdown\r\n')
+		{
+			channel.emit('shutdown');
+		}
 		channel.emit('broadcast', id, data);
+	});
+	
+	client.on('close', function() {
+		channel.emit('leave', id);
 	});
 });
 
